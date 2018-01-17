a1f13ab65bbfc1fdeebf844c8541c520530af5da
yGaByGa <ygabygabg@gmail.com>
2018-01-17 20:37:51 +0200

    upload all tasks

---
+++ C# Beginner/006.OperatorsAndExpressions/014.BitExchange.cs
@@ -0,0 +1,28 @@
+﻿//Write a program that exchanges bits 3, 4 and 5 with bits 24, 25 and 26 of given 32-bit unsigned integer(read from the console).
+
+using System;
+
+namespace _014.BitExchange
+{
+    class BitExchange
+    {
+        static void Main()
+        {
+            uint input = uint.Parse(Console.ReadLine());
+
+            uint mask1 = (1U << 3) | (1U << 4) | (1U << 5);
+            uint mask2 = (1U << 24) | (1U << 25) | (1U << 26);
+
+            uint modifiedInput = input & ~mask1 & ~mask2;
+
+            mask1 &= input;
+            mask1 <<= 21;
+            mask2 &= input;
+            mask2 >>= 21;
+            modifiedInput |= mask1 | mask2;
+
+            Console.WriteLine(modifiedInput);
+
+        }
+    }
+}
---
+++ C# Beginner/006.OperatorsAndExpressions/015.BitSwap.cs
@@ -0,0 +1,35 @@
+﻿//Write a program first reads 4 numbers n, p, q and k and than swaps bits {p, p+1, …, p+k-1} with bits 
+//{q, q+1, …, q+k-1} of n. Print the resulting integer on the console.
+
+using System;
+
+namespace _015.BitSwap
+{
+    class BitSwap
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            int q = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            for (int firstIndices = p, secondIndices = q, length = k;
+                (firstIndices < 32 && secondIndices < 32) && length > 0;
+                firstIndices++, secondIndices++, length--)
+            {
+                if (((n >> firstIndices) & 1) != ((n >> secondIndices) & 1))
+                {
+                    n = changeBits(n, firstIndices, secondIndices);
+                }
+            }
+            Console.WriteLine(n);
+        }
+
+        private static int changeBits(int number, int firstposition, int secondPosition)
+        {
+            number ^= (1 << firstposition);
+            return number ^ (1 << secondPosition);
+        }
+    }
+}
