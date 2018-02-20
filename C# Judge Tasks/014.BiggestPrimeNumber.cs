1dce328fc6706260ad3d8e558d40e505f4488bad
yGaByGa <ygabygabg@gmail.com>
2018-02-21 01:05:16 +0200

    upload task 014

---
+++ C# Judge Tasks/014.BiggestPrimeNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _014.BiggestPrimeNumber
+{
+    class BiggestPrimeNumber
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                bool isPrime = true;
+                int lenght = n / 2;
+
+                for (int i = 2; i < lenght; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(n);
+                    return;
+                }
+                else
+                {
+                    n -= 1;
+                }
+            }
+        }
+    }
+}
