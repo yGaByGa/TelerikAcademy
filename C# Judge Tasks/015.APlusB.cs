bd88dc491c753a96eaf99fd1120356ae0c106f57
yGaByGa <ygabygabg@gmail.com>
2018-02-27 23:36:08 +0200

    upload

---
+++ C# Judge Tasks/015.APlusB.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _015.APlusB
+{
+    class APlusB
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split();
+
+                int a = int.Parse(tokens[0]);
+                int b = int.Parse(tokens[1]);
+
+                Console.WriteLine(a + b);
+
+
+                //var arr = Console.ReadLine()
+                //.Split(' ')
+                //.Select(int.Parse)
+                //.ToArray();
+                //Console.WriteLine(arr[0] + arr[1]);
+            }
+        }
+    }
+}
